f7d16582790a1eb7a533d583e5e4ad259926b4f9
elcajas <solucion_k@outlook.com>
2023-06-29 18:25:54 +0900

    added new environment

---
+++ RewardEnv/dummy_env.py
@@ -0,0 +1,32 @@
+import pickle
+import pathlib
+import numpy as np
+
+dir_path = pathlib.Path(__file__).parent.resolve()
+
+## DUMMY ENV
+class MineEnv:
+    def __init__(self):
+        self.val = 0
+        self._elapsed_steps = 0
+        self._episode_len = 500
+        with open(dir_path.joinpath("observation_dict.pkl"), 'rb') as f:
+            self.obs = pickle.load(f)
+
+    def reset(self):
+        self._elapsed_steps = 0
+        return self.obs
+
+    def step(self, action):
+        self._elapsed_steps += 1
+        mu, sigma = 0, 0.01
+
+        obs = self.obs
+        reward = 1 + np.random.normal(mu, sigma)
+        done = False
+        info = None
+
+        return obs, reward, done, info
+
+    def close(self):
+        return None
